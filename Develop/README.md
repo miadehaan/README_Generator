8d236cf8596dd20da6c16519b41d034cb6e88d8a
miadehaan <64979457+miadehaan@users.noreply.github.com>
2020-09-13 17:57:08 -0700

    README file now gets generated after user prompts

---
+++ Develop/README.md
@@ -0,0 +1,19 @@
+
+
+    # proj1
+
+    # Table of Contents
+
+    # Table of Content
+    -[description](#description)
+    -[installation](#installation)
+    -[usage](#usage)
+    -[licenses](#licenses)
+    -[contribution](#contribution)
+    -[test](#test)
+    -[username](#username)
+    -[githubEmail](#githubEmail)
+
+
+
+    
--- Develop/index.js
+++ Develop/index.js
@@ -1,63 +1,120 @@
 var inquirer = require("inquirer");
 var fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile); // make a promise
 
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Provide a description of your project: ",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What are the installation instructions?",
+        name: "installation instructions"
+    },
+    {
+        type: "input",
+        message: "How will this project be used?",
+        name: "usage"
+    },
+    {
+        type: "list",
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+        name: "license"
+    },
+    { 
+        type: "input",
+        message: "Who contributed to this project?",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        message: "What are the test instructions for this project?",
+        name: "test"
+    },
+    {
+        type: "input",
+        message: "What is the name of the user?",
+        name: "userName"
+    },
+    {
+        type: "input",
+        message: "What is your Github email address?",
+        name: "githubEmail"
+    }
 
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(response) {
+    // Use Template Literals to write in the README file
+    return `
+    # ${response.title}
+
+    #Description 
+    ${response.description}
+
+    # Table of Content
+    -[description](#description)
+    -[installation](#installation)
+    -[usage](#usage)
+    -[licenses](#licenses)
+    -[contribution](#contribution)
+    -[test](#test)
+    -[username](#username)
+    -[githubEmail](#githubEmail)
+
+    #Installation
+    
+    #Usage
+    
+    #License
+    
+    #Contributing
+    
+    #Tests
+    
+    #Questions
+
+
+    `;
+
 }
 
 // function to initialize program
 function init() {
     inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "What is the title of your project?",
-            name: "title"
-        },
-        {
-            type: "input",
-            message: "Provide a description of your project: ",
-            name: "description"
-        },
-        {
-            type: "input",
-            message: "What are the installation instructions?",
-            name: "installation instructions"
-        },
-        {
-            type: "input",
-            message: "Please provide usage information: ",
-            name: "usage"
-        },
-        {
-            type: "list",
-            name: "license",
-            message: "What kind of license should your project have?",
-            choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
-        },
-        { 
-            type: "input",
-            message: "Please provide contribution guidelines: ",
-            name: "contribution"
-        },
-        {
-            type: "input",
-            message: "Please provide test instructions: ",
-            name: "test instructions"
+    .prompt(questions)
+    .then( (response) => {
+        // Write file w/ user answers
+        console.log(response);
 
-        }
-    ])
-    .then(function(response) {
         // Call function to write the file
-        // writeToFile(fileName, data);
+        const readme = writeToFile(response);
+        return writeFileAsync("README.md", readme);
+
+
 
         // Create badge specific to repo: https://shields.io
 
+    })
+    .then(function() {
+        // Promise received
+        console.log("Successfully wrote to README.md");
+    })
+    .catch(function(err) {
+        // Promise rejected
+        console.log(err);    
     });
 
 }
